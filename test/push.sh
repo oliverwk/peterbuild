03ecf99d45dfc4d3f73936172b06fa93419a3be4
oliverwk <30601311+oliverwk@users.noreply.github.com>
2021-04-16 07:32:15 +0000

    Restrucerd the repo

--- test/push.sh
+++
@@ -1,5 +0,0 @@
-git add /Users/MWK/Desktop/peterbuild/
-git commit -m "Fixed some cnetering problems"
-git branch -M master
-git remote add origin https://github.com/oliverwk/peterbuild.git
-git push -u origin master
